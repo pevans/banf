9b2de64b562944f2bb3ae2b64c8df7c66398c4eb
Peter Evans <peter@modul8r.com>
2022-09-05 20:06:44 -0500

    Support verbose flag and blog fail/warn

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/pevans/banf/blog"
 	"github.com/pevans/banf/bnf"
 	"github.com/rotisserie/eris"
 )
@@ -12,57 +13,55 @@ import (
 var grammarFile = flag.String("g", "",
 	"a file that defines a set of rules for a BNF grammar")
 
+var verboseFlag = flag.Bool("v", false,
+	"provide verbose description of what's happening")
+
 func main() {
 	flag.Parse()
 
 	if *grammarFile == "" {
-		fail(eris.New("a grammar file must be supplied"))
+		blog.Fail(eris.New("a grammar file must be supplied"))
+	}
+
+	if *verboseFlag {
+		blog.Verbosity++
 	}
 
 	gfile, err := os.Open(*grammarFile)
 	if err != nil {
-		fail(eris.Wrap(err, "couldn't open grammar file"))
+		blog.Fail(eris.Wrap(err, "couldn't open grammar file"))
 	}
 
 	stream, perr := bnf.Tokenize(gfile)
 	if perr != nil {
 		perr.File = *grammarFile
 		perr.Err = eris.Wrap(perr.Err, "couldn't tokenize grammar")
-		fail(perr)
+		blog.Fail(perr)
 	}
 
 	gram, err := bnf.NewGrammar(stream)
 	if err != nil {
-		fail(eris.Wrap(err, "couldn't define grammar"))
+		blog.Fail(eris.Wrap(err, "couldn't define grammar"))
 	}
 
 	for _, infile := range flag.Args() {
 		bytes, err := os.ReadFile(infile)
 		if err != nil {
-			fail(eris.Wrapf(err, "couldn't read file %s", infile))
+			blog.Fail(eris.Wrapf(err, "couldn't read file %s", infile))
 		}
 
 		perr := gram.Match(string(bytes))
 		if perr != nil && perr.Err != nil {
 			perr.File = infile
 			perr.Err = eris.Wrapf(perr.Err, "match attempt for %s failed with error", infile)
-			warn(perr)
+			blog.Warn(perr)
 		}
 
 		if perr == nil {
 			fmt.Printf("%s matches\n", infile)
 		} else {
 			fmt.Printf("%s does not match\n", infile)
-			warn(perr)
+			blog.Warn(perr)
 		}
 	}
-}
-
-func fail(err error) {
-	fmt.Fprintln(os.Stderr, eris.ToString(err, true))
-	os.Exit(1)
-}
-
-func warn(err error) {
-	fmt.Fprintln(os.Stderr, eris.ToString(err, true))
 }
